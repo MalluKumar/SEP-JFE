030b57c34c87b9d9eccd1755ee6506eb3ac20968
MalluMalligereKumar <mallumkumar069@gmail.com>
2021-04-12 15:03:47 +1000

    Change leaflet base map to CartoDB positron base map

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const App = () => {
   return (
-    <MapContainer id="container" center={[-33.865143, 151.209900]} zoom={13} scrollWheelZoom={true} >
-      <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap </a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    <MapContainer id="container" center={[-33.865143, 151.209900]} zoom={15} scrollWheelZoom={true} >
+      <TileLayer attribution='&copy; <a href="&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png" />
       <Marker position={[-33.865143, 151.209900]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
