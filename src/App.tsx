b44fcd7e3e74d2e1b8e8c8a57c3acfa83362dc47
Kunal Pahuja <S3608028@student.rmit.edu.au>
2021-04-15 16:49:01 +0500

    date and time added

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ const App = () => {
     const [middlePoints, setMiddlePoints] = React.useState(null);
     const [first, setFirst] = React.useState(null);
     const [last, setLast] = React.useState(null);
-    const [dateAndTime, setDateAndTime] = React.useState(null);
+    const [date, setDate] = React.useState(null);
+    const [time, setTime] = React.useState(null);
 
     useEffect(() => {
 
@@ -16,7 +17,8 @@ const App = () => {
             const dateAndTime = data[0]['F-START DATE TIME'];
             const LatLngData = JSON.parse(data[0]['L-POINTS IN TRIP']);
             const n = LatLngData.length;
-            setDateAndTime(dateAndTime.split('T')[1])
+            setDate(dateAndTime.split('T')[0])
+            setTime(dateAndTime.split('T')[1])
             setFirst(LatLngData[0]);
             setLast(LatLngData[n - 1]);
             LatLngData.shift();  // Removes the first element from an array and returns only that element.
@@ -31,7 +33,7 @@ const App = () => {
 
     return (
         <MapContainer id="container" center={[-33.900, 151.150]} zoom={15} scrollWheelZoom={true}>
-            <p style={{position: 'absolute', top: 5, right: 5, zIndex: 1000}}>{dateAndTime}</p>
+            <p style={{position: 'absolute', top: 5, right: 5, zIndex: 1000, fontSize: 16, fontWeight: "bold"}}>{`Date: ${date}, Time: ${time}`}</p>
             <TileLayer
                 attribution='&copy; <a href="&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                 url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"/>
