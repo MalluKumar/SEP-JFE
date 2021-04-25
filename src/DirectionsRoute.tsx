ad5fb5b71486b69e08d9b9569d1d322229b84f84
Kunal <54241991+kunalpahuja@users.noreply.github.com>
2021-04-26 02:45:54 +1000

    Update DirectionsRoute.tsx

--- src/DirectionsRoute.tsx
+++ src/DirectionsRoute.tsx
@@ -1,17 +1,21 @@
 import React, { Component, useEffect } from "react";
 import L, {LatLngTuple} from "leaflet";
 import {Marker, Popup, useMap} from "react-leaflet";
+// there is no typres of leaflet ant path so we have to ignore ts
 //@ts-ignore
 import {antPath} from 'leaflet-ant-path';
+// props interface
 interface IDirectionProps {
     coords: number[][],
     sdtid: string,
 }
 function DirectionsRoute(props: any) {
+    // leaflet map instance import
     const map = useMap();
     const {coords, sdtid} = props;
     useEffect(() => {
         // console.log(props)
+        // ant path instance
         const path = antPath(coords, {
             "delay": 400,
             "dashArray": [
@@ -25,15 +29,18 @@ function DirectionsRoute(props: any) {
             "reverse": false,
             "hardwareAccelerated": true
         });
+        // add ant path layer
         map.addLayer(path);
+        // fit to bound
         map.fitBounds(path.getBounds())
     }, [coords]);
-
+    // last coordinate of each array/job
     const position: LatLngTuple = coords[coords.length - 1];
     // const myIcon = L.icon({
     //     iconUrl: "icon_33997.svg",
     //     // iconAnchor: pinAnchor
     // });
+//     marker  of last point in array/means last point of route and popup for marker
     return <Marker draggable={true}
                    position={position}><Popup>{'GSTID: ' + sdtid + ', Location: ' + position}</Popup></Marker>;
 }
