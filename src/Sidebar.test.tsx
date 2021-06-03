c05920d5c83691540b788f93c689d30c6839cfb0
shimlaite <54255999+shimlaite@users.noreply.github.com>
2021-06-03 16:56:51 +1000

    Update Sidebar.test.tsx
    Fixed changes as per modified project code

--- src/Sidebar.test.tsx
+++ src/Sidebar.test.tsx
@@ -6,12 +6,13 @@ it('test correctly when sidebar is rendered', () => {
   const currentDateTime = new Date(2021, 10, 1, 13, 10, 10);
   const complianceRate = 100;
   const distanceTravelled = 0;
+  const jobsCompleted = 0;
   const timeOnJobs = 5;
 
   const updateRate = (speed: number) => {
     return;
   };
 
-  const tree = render(<Sidebar currentDateTime={currentDateTime} complianceRate={complianceRate} distanceTravelled={distanceTravelled} timeOnJobs={timeOnJobs} updateRate={updateRate} />);
+  const tree = render(<Sidebar currentDateTime={currentDateTime} complianceRate={complianceRate} distanceTravelled={distanceTravelled} timeOnJobs={timeOnJobs} jobsCompleted={jobsCompleted} updateRate={updateRate} />);
   expect(tree).toMatchSnapshot();
 });
